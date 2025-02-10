fddfd49e6a7535eeed87af723409d7e925f18472
Jay Johnson <jajohns721@icloud.com>
2025-02-10 16:45:26 -0600

    more blue shift

--- TeamProject1/src/main/java/com/example/teamproject1/filters/BlueShift.java
+++ TeamProject1/src/main/java/com/example/teamproject1/filters/BlueShift.java
@@ -12,12 +12,12 @@ public class BlueShift extends Filter{
     public BufferedImage applyFilter(File inputFile) throws IOException {
 
         // this is a variable for the new buffered image
-        BufferedImage loadedImage = ImageIO.read(inputFile);
+        BufferedImage inputImage = ImageIO.read(inputFile);
 
         // so we estentially have to loop through the x and y values so it can count every inch of the picture
-        for (int y = 0; y < loadedImage.getHeight(); y++) {
-            for (int x = 0; x < loadedImage.getWidth(); x++) {
-                int pixel = loadedImage.getRGB(x, y);
+        for (int y = 0; y < inputImage.getHeight(); y++) {
+            for (int x = 0; x < inputImage.getWidth(); x++) {
+                int pixel = inputImage.getRGB(x, y);
                 Color color = new Color(pixel);
                 
                 // we get color values :) 
@@ -27,10 +27,10 @@ public class BlueShift extends Filter{
                 int blue = 255; // blue value
 
                 int newColor = (alpha<<24) | (red<<16) | (green<<8) | blue;
-                loadedImage.setRGB(x, y, newColor);
+                inputImage.setRGB(x, y, newColor);
             }
         }
-        return loadedImage;
+        return inputImage;
     }
 } 
 
